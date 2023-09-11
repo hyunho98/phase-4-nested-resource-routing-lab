501abb955946e3e03df7ed6bf6e446990138c2ba
Alex Lee <hyunho98@gmail.com>
2023-09-11 19:16:29 -0400

    commit tree 5c771f45df7366231039607fd2072e3c0cced9bf

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,36 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response 
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = Item.create(item_params)
+    user.items << item
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def record_not_found_response
+    render json: { error: "User not found" }, status: :not_found
   end
 
 end
